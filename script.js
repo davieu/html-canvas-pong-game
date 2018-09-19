20fd20882c6eff8408aba6b3f0c3d9b1206600f5
davieu <davisumana@gmail.com>
2018-09-18 22:54:24 -0400

    ball reset if misses paddle and ball reflects if hits paddle

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ ballSpeedX = 15;
 ballSpeedY = 15;
 
 var paddle1Y = 250;
+var paddle2Y = 250;
 const PADDLE_HEIGHT = 100;
 
 function calculateMousePos(evt) {
@@ -35,18 +36,33 @@ window.onload = function() {
     });
 };
 
+function ballReset() {
+    ballSpeedX = -ballSpeedX;
+    ballX = canvas.width/2;
+    ballY = canvas.height/2;
+}
+
 function moveEverything() {
     ballX = ballX + ballSpeedX;
     ballY = ballY + ballSpeedY;
+
+    if (ballX < 0) {
+        if (ballY > paddle1Y && ballY < paddle1Y + PADDLE_HEIGHT) {
+            ballSpeedX = -ballSpeedX;
+        } else {
+            ballReset();
+        }
+    };
+
     if (ballX > canvas.width) {
-        ballSpeedX = -ballSpeedX;
-    } else if (ballX < 0) {
         ballSpeedX = -ballSpeedX;
     }
 
     if (ballY > canvas.height) {
         ballSpeedY = -ballSpeedY;
-    } else if (ballY < 0) {
+    } 
+    
+    if (ballY < 0) {
         ballSpeedY = -ballSpeedY;
     }
 }
@@ -57,6 +73,9 @@ function drawEverything() {
 
     //this is the left player paddle
     colorRect(20, paddle1Y, 10, PADDLE_HEIGHT, 'white');
+
+    //this is the left player paddle
+    colorRect(770, paddle2Y, 10, PADDLE_HEIGHT, 'white');
 
     //draws the ball
     colorCircle(ballX, ballY, 10, 'white');
